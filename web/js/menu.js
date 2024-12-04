419ebac704397af8016b46075dc1733846752b2d
tanzt <zhiting233071-dasein.edu.my>
2024-12-05 03:58:24 +0800

    menubar

---
+++ web/js/menu.js
@@ -0,0 +1,21 @@
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .ri-menu-line");
+let menuCloseBtn = document.querySelector(".nav-links .ri-close-line");
+menuOpenBtn.onclick = function () {
+    navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+    navLinks.style.left = "-100%";
+}
+
+// sidebar submenu open close js code
+let aboutArrow = document.querySelector(".about-arrow");
+aboutArrow.onclick = function () {
+    navLinks.classList.toggle("show1");
+}
+
+let liveNewsArrow = document.querySelector(".live-news-arrow");
+liveNewsArrow.onclick = function () {
+    navLinks.classList.toggle("show2");
+}
